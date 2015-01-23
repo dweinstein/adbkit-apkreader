3aedb7f10cbf778abf988e15813fb11c9124f815
David Weinstein <dweinst@insitusec.com>
2015-01-23 16:52:44 -0500

    simplify index

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 (function() {
-  var Path;
-
-  Path = require('path');
 
   module.exports = (function() {
-    switch (Path.extname(__filename)) {
-      case '.coffee':
-        return require('./src/apkreader');
-      default:
-        return require('./lib/apkreader');
-    }
+    return require('./lib/apkreader');
   })();
 
 }).call(this);
